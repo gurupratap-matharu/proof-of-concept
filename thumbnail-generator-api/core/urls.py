7a40620fdae432c46791357ba5134b5f3aed017c
guru <guru@q10mkt.com>
2019-04-07 19:35:42 -0300

    Schema endpoint added so that we can see the schema directly in the browser.

--- thumbnail-generator-api/core/urls.py
+++ thumbnail-generator-api/core/urls.py
@@ -3,7 +3,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from rest_framework.schemas import get_schema_view
 from rest_framework import routers
+
 from core.api import views
 
 router = routers.DefaultRouter()
@@ -11,11 +13,15 @@ router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 router.register(r'profile', views.ProfileViewSet)
 
+# initialize schema for server side documentation
+schema_view = get_schema_view(title='Thumbnail API')
+
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path('schema/', schema_view)
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
