1ec8e04d895715c44c53355d21cdeb0bc9391c81
guru <guru@q10mkt.com>
2019-04-08 13:06:55 -0300

    Adding the requirements.txt file

--- thumbnail-generator-api/core/urls.py
+++ thumbnail-generator-api/core/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from rest_framework.documentation import include_docs_urls
 from rest_framework.schemas import get_schema_view
 from rest_framework import routers
 
@@ -14,14 +15,16 @@ router.register(r'groups', views.GroupViewSet)
 router.register(r'profile', views.ProfileViewSet)
 
 # initialize schema for server side documentation
-schema_view = get_schema_view(title='Thumbnail API')
+schema_view = get_schema_view(title='Thumbnail API',
+                              description='An API that generates thumbnails for users in 3 different formats.')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('schema/', schema_view)
+    path('schema/', schema_view),
+    path('docs/', include_docs_urls(title='Thumbnail Generator API'))
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
